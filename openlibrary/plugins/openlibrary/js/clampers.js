cd60705047c0c25316aa0a07a9e6897f5d02af84
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2026-02-14 22:01:22 -0800

    Fix clamper class removal and event handling in clampers.js for improved functionality

--- openlibrary/plugins/openlibrary/js/clampers.js
+++ openlibrary/plugins/openlibrary/js/clampers.js
@@ -5,7 +5,7 @@
 export function initClampers(clampers) {
     for (const clamper of clampers) {
         if (clamper.clientHeight === clamper.scrollHeight) {
-            clamper.classList.remove('.clamp');
+            clamper.classList.remove('clamp');
         } else {
 
             /*
@@ -14,20 +14,16 @@ export function initClampers(clampers) {
             */
 
             clamper.addEventListener('click', (event) => {
-                const up = event.target;
-
                 if (event.target instanceof HTMLAnchorElement) {
                     return;
                 }
 
-                if (up.classList.contains('clamp')) {
-                    clamper.style.display = clamper.style.display === '-webkit-box' || clamper.style.display === '' ? 'unset' : '-webkit-box'
+                clamper.style.display = clamper.style.display === '-webkit-box' || clamper.style.display === '' ? 'unset' : '-webkit-box'
 
-                    if (up.getAttribute('data-before') === '\u25BE ') {
-                        up.setAttribute('data-before', '\u25B8 ')
-                    } else {
-                        up.setAttribute('data-before', '\u25BE ')
-                    }
+                if (clamper.getAttribute('data-before') === '\u25BE ') {
+                    clamper.setAttribute('data-before', '\u25B8 ')
+                } else {
+                    clamper.setAttribute('data-before', '\u25BE ')
                 }
             })
         }
